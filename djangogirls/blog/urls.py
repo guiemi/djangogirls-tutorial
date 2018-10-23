ddba5afaa6babcffc51b2a3046919048d4e14674
guiemi <guiemi.teixeira@gmail.com>
2018-10-23 20:58:03 -0300

    Botando essa bagaça no ar novamente.

--- djangogirls/blog/urls.py
+++ djangogirls/blog/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url('post/<int:pk>/', views.post_detail, name='post_detail'),
-    url('post/new', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    # url(r'^$', views.post_list, name='post_list'),
+    # url('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # url('post/new', views.post_new, name='post_new'),
+    # url('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
+    path('', views.post_list, name='list_list'),
+    path('post/<int:pk/>', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit', views.post_edit, name='post_edit'),
 
 ]
